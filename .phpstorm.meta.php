c0f3b8b60686050c5bb38f4d03a04b9cb63c05a9
ZYCGary <zyc.gary@gmail.com>
2020-02-08 18:28:47 +1100

    Generate authentication framework Add multi-language package

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2020-02-05 17:05:58.
+    * Generated on 2020-02-08 18:07:18.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
--- _ide_helper.php
+++ _ide_helper.php
@@ -3,7 +3,7 @@
 
 /**
  * A helper file for Laravel 5, to provide autocomplete information to your IDE
- * Generated for Laravel 6.14.0 on 2020-02-05 17:05:57.
+ * Generated for Laravel 6.14.0 on 2020-02-08 18:07:17.
  *
  * This file should not be included in your code, only analyzed by your IDE!
  *
