cea52c38dc0c69dfa3c531a86142abc5ff3ddd48
ZYCGary <zyc.gary@gmail.com>
2020-02-10 15:13:00 +1100

    Mix

--- public/js/app.js
+++ public/js/app.js
@@ -37112,15 +37112,27 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 /***/ }),
 
+/***/ "./resources/sass/main.scss":
+/*!**********************************!*\
+  !*** ./resources/sass/main.scss ***!
+  \**********************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/css-loader/index.js):\nModuleBuildError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\nError: ENOENT: no such file or directory, open '/Users/gary/code/airsyd/resources/sass/main.scss'\n    at /Users/gary/code/airsyd/node_modules/webpack/lib/NormalModule.js:316:20\n    at /Users/gary/code/airsyd/node_modules/loader-runner/lib/LoaderRunner.js:367:11\n    at /Users/gary/code/airsyd/node_modules/loader-runner/lib/LoaderRunner.js:203:19\n    at /Users/gary/code/airsyd/node_modules/enhanced-resolve/lib/CachedInputFileSystem.js:85:15\n    at processTicksAndRejections (internal/process/task_queues.js:79:11)");
+
+/***/ }),
+
 /***/ 0:
-/*!*************************************************************!*\
-  !*** multi ./resources/js/app.js ./resources/sass/app.scss ***!
-  \*************************************************************/
+/*!****************************************************************************************!*\
+  !*** multi ./resources/js/app.js ./resources/sass/app.scss ./resources/sass/main.scss ***!
+  \****************************************************************************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(/*! /Users/gary/code/airsyd/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /Users/gary/code/airsyd/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /Users/gary/code/airsyd/resources/sass/app.scss */"./resources/sass/app.scss");
+module.exports = __webpack_require__(/*! /Users/gary/code/airsyd/resources/sass/main.scss */"./resources/sass/main.scss");
 
 
 /***/ })
